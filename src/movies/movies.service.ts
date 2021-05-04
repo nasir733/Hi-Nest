684fe13bd929c8c7877b1c2db9fdb5db97f347c9
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-05-04 06:32:06 +0500

    learned
    how to take body params and query from the request

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,7 +10,11 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie= this.movies.find((movie) => movie.id === +id);
+    if(!movie){
+      throw new NotFoundException("Movie with id not found");
+    }
+    return movie
   }
 
   deleteOne(id: string): boolean {
@@ -23,5 +27,10 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+  update(id:string,updateData){
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({...movie,...updateData})
   }
 }
